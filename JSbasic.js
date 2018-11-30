83238b0f605a73c6d5fb03aad15ebc47f949391e
JustinNealHatch <34749032+JustinNealHatch@users.noreply.github.com>
2018-11-30 11:37:42 -0800

    Add files via upload

---
+++ JSbasic.js
@@ -0,0 +1,28 @@
+var my_favorite_number = 27;
+console.log(my_favorite_number/2);
+
+var someones_favorite = 7;
+console.log(someones_favorite);
+
+var a = my_favorite_number - someones_favorite;
+console.log(a);
+
+var b = my_favorite_number * 26;
+console.log(b);
+
+var str = "Please locate where 'locate' occurs!";
+var pos = str.indexOf("where");
+console.log(pos);
+
+var num = 7689;
+var num1 = str.indexOf(9);
+console.log(num1);
+
+var k = "Hello Enrique".length;
+console.log(k);
+
+var f = [1,2,3,4,5].length;
+console.log(f);
+
+var t = 0/12;
+console.log(t);
